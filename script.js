1b4f43ef65b1d57c4216474d0a0e1b5c029a374a
Edan Schwartz <edanschwartz@gmail.com>
2020-12-17 12:49:41 -0600

    Conditionals!

---
+++ script.js
@@ -0,0 +1,28 @@
+console.log('Hello Cullen!');
+
+const howManyKidsDoIHave = 1;
+// howManyKidsDoIHave = 4;
+console.log('How many kids do I have', howManyKidsDoIHave);
+
+
+let doIHaveLotsOfKids = howManyKidsDoIHave >= 2;
+console.log('doIHaveLotsOfKids?', doIHaveLotsOfKids);
+if (doIHaveLotsOfKids) {
+  console.log('ITS A MADHOUSE HERE.')
+}
+else if (howManyKidsDoIHave === 1) {
+  console.log('The first is hard.');
+}
+
+
+let canPigsFly = false;
+
+if (canPigsFly) {
+  // canPigsFly === true
+  // false === true
+  // false
+  console.log('watch your head');
+}
+else {
+  console.log('Reality is as I expect.')
+}
